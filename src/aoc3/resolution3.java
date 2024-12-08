1201afd05175cdc38e3513108489f731e15344cc
jakpalka <jpalka@dentalintel.com>
2024-12-08 16:31:55 +0100

    aoc3p2 resolution

--- src/aoc3/resolution3.java
+++ src/aoc3/resolution3.java
@@ -12,35 +12,47 @@ public class resolution3 {
 
     static File data = new File("src/aoc3/data");
     static ArrayList<String> mulStrings = new ArrayList<>();
+    static ArrayList<String> mulStringsSelected = new ArrayList<>();
 
 
     public static void main(String[] args) {
         analyzeData(data);
-        System.out.println(countSumOfMultiplications(mulStrings));
+        System.out.println("First part result: " + countSumOfMultiplications(mulStrings));
+        System.out.println("Second part result: " + countSumOfMultiplications(mulStringsSelected));
 
     }
 
     public static void analyzeData(File dataFile) {
         try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
             String line;
+            StringBuilder longString = new StringBuilder();
             while ((line = reader.readLine()) != null) {
-                analyzeTextLine(line);
+                longString.append(line);
             }
-
+            analyzeTextLine(longString.toString(), mulStrings);
+            analyzeTextLineBetter(longString.toString(), mulStringsSelected);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void analyzeTextLine(String textLine) {
+    public static void analyzeTextLine(String textLine, ArrayList<String> results) {
         Pattern pattern = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)");
         Matcher matcher = pattern.matcher(textLine);
         while (matcher.find()) {
-            mulStrings.add(matcher.group());
+            results.add(matcher.group());
         }
     }
 
-    public static int countSumOfMultiplications(ArrayList<String> stringsArrayList){
+    public static void analyzeTextLineBetter(String textLine, ArrayList<String> results) {
+        String[] doSplitedStrings = textLine.split("do\\(\\)");
+        for (String s : doSplitedStrings) {
+            String[] dontSplitedStrings = s.split("don't\\(\\)");
+            analyzeTextLine(dontSplitedStrings[0], results);
+        }
+    }
+
+    public static int countSumOfMultiplications(ArrayList<String> stringsArrayList) {
         int sum = 0;
         for (String s : stringsArrayList) {
             int[] multiplicationFactors = new int[2];
